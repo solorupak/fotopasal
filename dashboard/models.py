a645baebc7d34ce68079bf7a1b6778cd1edd626e
unknown <devs44sunuwar@gmail.com>
2021-07-01 10:21:01 +0545

    models

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,5 +1,6 @@
 from django.apps import apps
 from django.db import models
+from django.db.models.fields import DateField
 from django.utils import timezone
 
 
@@ -43,3 +44,42 @@ class Designation(DateTimeModel):
     def __str__(self):
         return self.name
 
+# class Events(DateTimeModel):
+#     title = models.CharField(max_length=200)
+
+# class Package(DateTimeModel):
+#     title = models.CharField(max_length=200)
+
+# class FamilyNames(DateTimeModel):
+#     relation = models.CharField(max_length=200)
+#     full_name = models.CharField(max_length=200)
+
+# class Photographer(DateTimeModel):
+#     full_name = models.CharField(max_length=200)
+
+# class VideoGrapher(DateTimeModel):
+#     full_name = models.CharField(max_length=200)
+
+
+# class Data(DateTimeModel):
+#     bill_number = models.PositiveBigIntegerField()
+#     client_name = models.CharField(max_length=200)
+#     contact_no = models.CharField(max_length=200)
+#     events = models.ForeignKey(Events, on_delete=models.CASCADE)
+#     date = models.DateField()
+#     package = models.ForeignKey(Package, on_delete=models.CASCADE)
+#     total_price = models.PositiveBigIntegerField()
+#     advance = models.PositiveBigIntegerField()
+#     due =  models.PositiveBigIntegerField()
+#     photo_selection = models.BooleanField()
+#     # songs 
+#     family_names = models.ForeignKey(FamilyNames, on_delete=models.CASCADE)
+#     photographer = models.ForeignKey(Photographer, on_delete=models.CASCADE)
+#     videographer = models.ForeignKey(VideoGrapher, on_delete=models.CASCADE)
+#     #backup1
+#     #backup2
+#     deadline = models.DateField()
+#     #workstatus
+#     copied_to = models.CharField(max_length=200)
+#     delivery_status = models.CharField(max_length=200, choices=DELIVERY STATUS)
+    
